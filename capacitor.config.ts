69ab68057321b1dc782f230e5f5c17b6bfd59a38
rakshitharamachandra <132129413+rakshitharamachandra@users.noreply.github.com>
2024-08-02 17:17:50 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pratice_mob',
+  webDir: 'www'
+};
+
+export default config;
