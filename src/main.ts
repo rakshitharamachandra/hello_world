8166cfde9e4e7bfb65bbd3aec423043a7056de63
rakshitharamachandra <132129413+rakshitharamachandra@users.noreply.github.com>
2024-08-07 17:56:55 +0530

    fourth commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { Capacitor } from '@capacitor/core';
+import { initializeApp } from 'firebase/app';
+import { getAuth, indexedDBLocalPersistence, initializeAuth } from 'firebase/auth';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
+
+function provideAuth() {
+  if (Capacitor.isNativePlatform()) {
+    return initializeAuth(initializeApp({}), { // Initialize Firebase App with your config
+      persistence: indexedDBLocalPersistence
+    });
+  } else {
+    return getAuth(initializeApp({})); // Initialize Firebase App with your config
+  }
+}
